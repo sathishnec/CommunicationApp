a8671b4772bab0f226be5d16e34ef1d1b3969fc1
Sathish <sathish_nec@yahoo.co.in>
2013-10-11 16:27:26 +0100

    Added the Users Module to the application.config.php file. Probably might have done a undo at some point.

--- module/Users/src/Users/Controller/IndexController.php
+++ module/Users/src/Users/Controller/IndexController.php
@@ -3,7 +3,6 @@
 namespace Users\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-
 class IndexController extends AbstractActionController
 {
 	public function indexAction()
